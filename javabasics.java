341e0184c2a0fad19ca871a4c7cdd07c3e414a50
AbhiRathour7 <abhi.rathourvns@gmail.com>
2025-01-19 17:26:11 +0530

    javaBasics

---
+++ javabasics.java
@@ -0,0 +1,88 @@
+import java.util.*;
+
+public class javabasics {
+    public static void main(String[] args) {   
+    Scanner sc = new Scanner(System.in);
+    int a =10;
+    float b = 20.25f;
+    long c = 25;
+    double d =30;
+    double ans = a+b+c+d;
+    System.out.println("answer:" +ans);    
+    //    char a ='a';
+    //    char b = 'b';
+    //    System.out.println((int)b);
+    //    System.out.println((int)a);
+    //    System.out.println(b-a);
+    }
+}
+
+
+
+//TYPE CASTING.
+// import java.util.*;
+
+// public class javabasics {
+//     public static void main(String[] args) {   
+//     Scanner sc = new Scanner(System.in);
+//     float marks1 = 99.12f;
+//     int marks2 = (int)marks1;
+//     char ch1 ='a';
+//     char ch2 ='b';
+//     int a = ch1;
+//     int b = ch2;
+//     System.out.println(a); 
+//     System.out.println(b); 
+//     System.out.println(marks2);
+//     }
+// }
+
+//AREA OF CIRCLE.
+// import java.util.*;
+
+// public class javabasics {
+//     public static void main(String[] args) {   
+//     Scanner sc = new Scanner(System.in);
+//     System.out.println("enter the radius of circle");
+//     float r = sc.nextFloat();
+//     float area = 3.14f * r *r;
+//     System.out.println(area);
+
+//     }
+// }
+
+
+
+//PRODUCT OF TWO NUMBERS.
+// import java.util.Scanner;
+
+// public class javabasics {
+//     public static void main(String[] args) {   
+//     Scanner sc = new Scanner(System.in);
+//     System.err.println("enter the first number");
+//     int a = sc.nextInt();
+//     System.err.println("enter second number");
+//     int b = sc.nextInt();
+//     int product = a*b;
+//     System.out.println(product);
+
+//     }
+// }
+
+
+
+//SUM OF TWO NUMBERS.
+// import java.util.Scanner;
+
+// public class javabasics {
+//     public static void main(String[] args) {   //boilerplate code
+//     Scanner sc = new Scanner(System.in);
+//     System.err.println("enter the first number");
+//     int a = sc.nextInt();
+//     System.err.println("enter second number");
+//     int b = sc.nextInt();
+//     int sum = a+b;
+//     System.out.println(sum);
+
+//     }
+// }
