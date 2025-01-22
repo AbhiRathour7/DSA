1c14aacabfa2746a108bfdf9569dad97e6f429f7
AbhiRathour7 <abhi.rathourvns@gmail.com>
2025-01-22 22:39:43 +0530

    if-else

---
+++ conditional/conditional.java
@@ -0,0 +1,39 @@
+
+
+// NESTED IF-ELSE Condition.
+import java.util.Scanner;
+
+public class conditional {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the age");
+        int age = sc.nextInt();
+        if (age >=18){
+            System.out.println("you are eligible for voting");
+        }
+        if (age>13 && age<18){
+            System.out.println("you are teenager");
+        }
+        else{
+            System.out.println("you are not eligible for voting");
+        }
+    }  
+}
+
+
+// IF-ELSE Condition.
+// import java.util.Scanner;
+
+// public class conditional {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the age");
+//         int age = sc.nextInt();
+//         if (age >=18){
+//             System.out.println("you are eligible for voting");
+//         }
+//         else{
+//             System.out.println("you are not eligible for voting");
+//         }
+//     }  
+// }
