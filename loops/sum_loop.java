2208eb4557ab6db119242614e850856683206bd2
AbhiRathour7 <abhi.rathourvns@gmail.com>
2025-01-27 21:03:14 +0530

    Sum of N naturals no.

--- loops/sum_loop.java
+++ loops/sum_loop.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 
-public class while_loop {
+public class sum_loop {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+            Scanner sc = new Scanner(System.in);
         System.out.println("enter the number");
         int n = sc.nextInt();
         int i=1;
+        int sum = 0;
         while(i<=n){
-            System.out.print(i+" ");
+            sum = sum + i;
             i++;
         }
-    }
+            System.out.println("sum is :" +sum);
+    
+}
 }
