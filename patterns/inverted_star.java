28f68c386c18061819ed903ffebbd29db091a8e7
AbhiRathour7 <abhi.rathourvns@gmail.com>
2025-03-01 10:39:52 +0530

    Inverted_Star_pattern

--- patterns/inverted_star.java
+++ patterns/inverted_star.java
@@ -8,7 +8,7 @@ public class inverted_star {
         System.out.println("enter the number");
         int n = sc.nextInt();
         for(int i=1;i<=n;i++){
-            for(int j=n;j>=i;j--){
+            for(int j=1;j<=n-i+1;j++){
                 System.out.print("*");
             }
             System.out.println();
@@ -16,3 +16,23 @@ public class inverted_star {
         
     }
 }
+
+// METHOD 1
+// package patterns;
+
+// import java.util.Scanner;
+
+// public class inverted_star {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the number");
+//         int n = sc.nextInt();
+//         for(int i=1;i<=n;i++){
+//             for(int j=n;j>=i;j--){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+        
+//     }
+// }
