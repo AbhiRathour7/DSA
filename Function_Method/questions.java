8a903b972aea804df1a866115c5104193098adc9
AbhiRathour7 <abhi.rathourvns@gmail.com>
2025-03-05 21:29:14 +0530

    Factorial

--- Function_Method/questions.java
+++ Function_Method/questions.java
@@ -5,18 +5,45 @@ import java.util.*;
 public class questions {
     
 
-    public static int productOfTwo(int a,int b){
-        int prod=a*b;
-        return prod;
+    public static int factorial(int n){
+        int fact=1;
+        for(int i=1;i<=n;i++){
+            fact=fact*i;
+        }
+        return fact;
     }
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         System.out.println("enter first number:");
-        int n=sc.nextInt();
-        System.out.println("enter second number:");
-        int m=sc.nextInt();
-        int result =productOfTwo(n,m);
-        System.out.println("product is ="+ result);
+        int a=sc.nextInt();
+    
+       
+        System.out.println("factorial:"+factorial(a));
 
     }
 }
+
+// product of two numbers
+
+// package Function_Method;
+
+// import java.util.*;
+
+// public class questions {
+    
+
+//     public static int productOfTwo(int a,int b){
+//         int prod=a*b;
+//         return prod;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc =new Scanner(System.in);
+//         System.out.println("enter first number:");
+//         int n=sc.nextInt();
+//         System.out.println("enter second number:");
+//         int m=sc.nextInt();
+//         int result =productOfTwo(n,m);
+//         System.out.println("product is ="+ result);
+
+//     }
+// }
