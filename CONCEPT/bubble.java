afa600c68904730d4b2760955a5616e8077937de
AbhiRathour7 <abhi.rathourvns@gmail.com>
2025-06-23 20:27:03 +0530

    Bubble sort

---
+++ CONCEPT/bubble.java
@@ -0,0 +1,77 @@
+// public class bubble {
+//         public static void main(String[] args) {
+//         int num[] = {6,5,2,8,9,4};
+//         int size= num.length;
+//         int temp=0;
+//         System.out.println("before sorting : ");
+//         for(int n:num){
+//             System.out.print(n+ " ");
+//         }
+
+//             for(int i=0;i<size;i++){
+//                 for(int j=0; j<size-i-1;j++){
+//                     if(num[j]>num[j+1]){
+//                         temp=num[j];
+//                         num[j]=num[j+1];
+//                         num[j+1]=temp;
+//                     }
+//                 }
+//                 System.out.println();
+//                 for(int n:num){
+                    
+//                     System.out.print(n+ " ");
+//                 }
+//             }
+
+//             System.out.println();
+//             System.out.println("after sorting : ");
+//             for(int n:num){
+//             System.out.print(n+ " ");
+//         }
+        
+
+//     }
+// }
+
+public class bubble {
+        public static void main(String[] args) {
+        int  a[] ={6,5,2,8,3,7};
+        int size=a.length;
+        int mxi=-1;
+        int temp=0;
+        System.out.println("before sorting");
+        for(int n:a){
+            System.out.print(n+" ");
+        }
+        for(int i=0;i<size;i++){
+            mxi=i;
+            for(int j=mxi+1;j<size;j++){
+               
+                if(a[mxi]>a[j]){
+                    temp=a[j];
+                    a[j]= a[mxi];
+                    a[mxi]=temp;
+                     mxi =j;
+                }
+                else{
+                    mxi =j;
+                }
+                
+
+                System.out.println();
+                for(int n:a){
+                    System.out.print(n+" ");
+            }
+            }
+        }
+    
+
+        
+
+        System.out.println("after sorting");
+        for(int n:a){
+            System.out.print(n+" ");
+    }
+}
+}
+        
