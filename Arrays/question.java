76a983752c7860e1794ae3b226090742daffe64b
AbhiRathour7 <abhi.rathourvns@gmail.com>
2025-07-16 10:11:34 +0530

    ARRAY QUESTIONS

---
+++ Arrays/question.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+
+/*  Question 1: Given an integer array nums, return true if any value appears at least twice in the
+    array, and return false if every element is distinct.
+    Example 1:
+    Input: nums = [1, 2, 3, 1]
+    Output: true  */
+
+// package Arrays;
+
+// public class question {
+
+//     public static boolean same(int arr[]){    
+//         for(int i=0;i<arr.length;i++){
+//             for(int j=i+1;j<arr.length;j++){
+//                 if(arr[i]==arr[j]){
+//                    return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+//     public static void main(String[] args) {
+//         int arr[]={1,2,3,4};
+//         System.out.println(same(arr));
+//      }
+// }
+
